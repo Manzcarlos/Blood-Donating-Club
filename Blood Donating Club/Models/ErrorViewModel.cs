04c6fd5d4a61cc1ada25a3661b32ab91a7453c2b
cemh_ <cemh_@CARLOS>
2023-11-20 02:46:12 +0100

    Add project files.

---
+++ Blood Donating Club/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blood_Donating_Club.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
